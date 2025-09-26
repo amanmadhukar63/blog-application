f1daad18818c1de56ba80329f110df4e511881a6
Aman Madhukar <amanmadhukar@MacBook-Air.local>
2025-09-27 02:15:05 +0530

    user model created

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,37 @@
+import { Schema, model } from 'mongoose';
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true });
+
+// Password hashing before saving the user
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
