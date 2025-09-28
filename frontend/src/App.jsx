129764d1496575ff6ddd685ba1b97a389d8e30b0
Aman Madhukar <amanmadhukar@MacBook-Air.local>
2025-09-28 19:37:07 +0530

    global context and toaster intialized in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,46 +8,51 @@ import MyBlogs from './pages/MyBlogs';
 import BlogDetail from './pages/BlogDetail';
 import CreateBlog from './pages/CreateBlog';
 import EditBlog from './pages/EditBlog';
+import { Toaster } from 'react-hot-toast';
+import { GlobalContextProvider } from './context/GlobalContext.jsx';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-background text-foreground">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/blog/:id" element={<BlogDetail />} />
-            
-            <Route 
-              path="/my-blogs" 
-              element={
-                <ProtectedRoute>
-                  <MyBlogs />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/create-blog" 
-              element={
-                <ProtectedRoute>
-                  <CreateBlog />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/edit-blog/:id" 
-              element={
-                <ProtectedRoute>
-                  <EditBlog />
-                </ProtectedRoute>
-              } 
-            />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <GlobalContextProvider>
+      <Router>
+        <div className="min-h-screen bg-background text-foreground">
+          <Navbar />
+          <Toaster />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/explore" element={<Explore />} />
+              <Route path="/blog/:id" element={<BlogDetail />} />
+              
+              <Route 
+                path="/my-blogs" 
+                element={
+                  <ProtectedRoute>
+                    <MyBlogs />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/create-blog" 
+                element={
+                  <ProtectedRoute>
+                    <CreateBlog />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/edit-blog/:id" 
+                element={
+                  <ProtectedRoute>
+                    <EditBlog />
+                  </ProtectedRoute>
+                } 
+              />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </GlobalContextProvider>
   );
 }
 
