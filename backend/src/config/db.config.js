0b1b6e14590f79a41f3a691dbdca1fe7f5b74c71
Aman Madhukar <amanmadhukar@MacBook-Air.local>
2025-09-28 17:19:01 +0530

    mongodb url changed

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('🟢 DB connected succesfully!!!');
     }
     catch(error){
